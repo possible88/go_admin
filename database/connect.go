82d894bb0f029144e20d7e201dc088fcde02e55a
possible88 <ernestpossible@gmail.com>
2022-04-05 09:17:06 -0700

    Add files via upload

--- database/connect.go
+++ database/connect.go
@@ -1,1 +1,21 @@
+package database
 
+import (
+	"go_admin/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	database, err := gorm.Open(mysql.Open("root:5sntVLe69LSkrMK@tcp(db:3306)/go_admin?parseTime=true"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to database")
+	}
+
+	DB = database
+
+	database.AutoMigrate(&models.User{}, &models.Product{}, &models.Job{}, &models.Message{}, &models.Productimg{}, &models.Comment{})
+}
